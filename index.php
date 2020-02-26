2009dff923b8dac716f94336aa64791890627739
Nuno Pereira <nunocgpereira@gmail.com>
2020-02-26 23:35:05 +0000

    changes in side nav style

--- index.php
+++ index.php
@@ -4,22 +4,28 @@
 	<meta charset="utf-8">
 	<title>Notes Blog</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
 </head>
 <body>
 
 	<nav class="side-nav">
 		<ul>
-			<li>
-				<a href="">Test</a>
-			</li>
+			<li><a href=""><i class="fas fa-home"></i>Home</a></li>
+			<li><a href="" class="active"><i class="fas fa-user"></i>Profile</a></li>
+			<li><a href=""><i class="fas fa-list-ul"></i>Categories</a></li>
+			<li><a href=""><i class="fas fa-cog"></i>Settings</a></li>
 		</ul>
 	</nav>
 
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	<div class="main-container">
+		te
+	</div>
+
+	<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
 	<script type="text/javascript">
 		CKEDITOR.replace('editor')
-	</script>
+	</script> -->
 
 </body>
 </html>
